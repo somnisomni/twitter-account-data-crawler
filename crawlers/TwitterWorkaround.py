b6fcfbfff766335fe575bd181c3aea29a8f3eaab
somni <me@somni.one>
2023-08-23 08:19:26 +0900

    Error handling in Twitter workaround crawler

--- crawlers/TwitterWorkaround.py
+++ crawlers/TwitterWorkaround.py
@@ -20,25 +20,29 @@ class TwitterWorkaround(Twitter):
   def navigate(self):
     wait = WebDriverWait(self.driver, 5)
 
-    ''' Stage 1. Navigate to status page '''
-    log("Start navigating to Twitter status page (handle: @{}, status id: {}).".format(self.handle, self.status_id))
-    self.driver.get("https://twitter.com/{}/status/{}".format(self.handle, self.status_id))
+    try:
+      ''' Stage 1. Navigate to status page '''
+      log("Start navigating to Twitter status page (handle: @{}, status id: {}).".format(self.handle, self.status_id))
+      self.driver.get("https://twitter.com/{}/status/{}".format(self.handle, self.status_id))
 
-    ''' Stage 1-1. Wait for dynamic load '''
-    log("Waiting for dynamic load to be completed...")
-    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='primaryColumn']")))
-    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='User-Name']")))
+      ''' Stage 1-1. Wait for dynamic load '''
+      log("Waiting for dynamic load to be completed...")
+      wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='primaryColumn']")))
+      wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='User-Name']")))
 
-    ''' Stage 2. Navigate to profile page by clicking username anchor '''
-    log("Status page loaded, try finding and clicking profile link anchor...")
-    username_element = self.driver.find_element(By.CSS_SELECTOR, "[data-testid='User-Name']")
-    username_anchor_element = username_element.find_element(By.TAG_NAME, "a")
-    username_anchor_element.click()
+      ''' Stage 2. Navigate to profile page by clicking username anchor '''
+      log("Status page loaded, try finding and clicking profile link anchor...")
+      username_element = self.driver.find_element(By.CSS_SELECTOR, "[data-testid='User-Name']")
+      username_anchor_element = username_element.find_element(By.TAG_NAME, "a")
+      username_anchor_element.click()
 
-    ''' Stage 2-1. Wait for dynamic load '''
-    log("Waiting for dynamic load to be completed...")
-    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='primaryColumn']")))
-    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='UserName']")))
+      ''' Stage 2-1. Wait for dynamic load '''
+      log("Waiting for dynamic load to be completed...")
+      wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='primaryColumn']")))
+      wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "[data-testid='UserName']")))
+    except:
+      log("Seems like the page is not loaded correctly! Is the website updated, or Twitter blocked access?")
+      return
 
   def wait(self):
     # navigate() does wait, no need to declare something in here.
